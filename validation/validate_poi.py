60bf716c1d39cd83d0e2946102fb477cfd2c4aad
ssaporito <sandrosaporito@outlook.com>
2017-06-29 14:58:32 -0300

    Finished validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,5 +28,14 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+features_train,features_test,labels_train,labels_test=train_test_split(features,labels,test_size=0.3,random_state=42)
+
+clf=DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print clf.score(features_test,labels_test)
+
 
 
